de73dfb68b01479ab8357d9f72582c08998c44db
BobKat99 <ladu@deakin.edu.au>
2019-09-05 06:49:07 +1000

    new

---
+++ web/public/app.js
@@ -0,0 +1,15 @@
+
+$('#footer').load('footer.html');
+
+$('#progress').on('click', function() {  
+    location.href = 'progress.html'
+});
+$('#virtual-items').on('click', function() {  
+    location.href = 'virtual_items.html'
+});
+$('#package').on('click', function() {  
+    location.href = 'package.html'
+});
+$('#login').on('click', function() {  
+    location.href = 'stores.html'
+});
